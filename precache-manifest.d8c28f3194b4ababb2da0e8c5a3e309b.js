b19f0d0410d4325c43d88c5f7b983afdcfb934c5
Nicholas Yang <ny585@nyu.edu>
2020-01-04 22:13:16 -0500

    Updates

---
+++ precache-manifest.d8c28f3194b4ababb2da0e8c5a3e309b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d99ced7d9e8ae48e208958d32c39a54",
+    "url": "/courses/index.html"
+  },
+  {
+    "revision": "e72ef9b94f2f661db4f3",
+    "url": "/courses/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5147c1895865ef642a38",
+    "url": "/courses/static/js/2.fa216e01.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/courses/static/js/2.fa216e01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e72ef9b94f2f661db4f3",
+    "url": "/courses/static/js/main.3263ed9a.chunk.js"
+  },
+  {
+    "revision": "86c9d5da68bb83634875",
+    "url": "/courses/static/js/runtime-main.7381bc57.js"
+  }
+]);
