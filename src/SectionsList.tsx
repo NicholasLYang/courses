7cca792f5efefcb2e5baf2aa49e80877c66609d0
Kn99HN <khanhnguyen99hn@gmail.com>
2020-03-22 16:24:53 -0400

    maybe this will be more readable

--- src/SectionsList.tsx
+++ src/SectionsList.tsx
@@ -5,42 +5,29 @@ import Section from "./Section";
 import { ISection } from "./types";
 
 interface Props {
-  sections: Array<ISection>;
+  sections: Array<ISection> | null; 
   displayDescription: boolean;
   displayNotes: boolean;
 }
 
-interface Rec {
-  recitations : Array<ISection> | null;
-}
-
-const RecitationList : React.FC<Rec> = ({
-  recitations  
-}) => {
-  if(!recitations) {
-    return(<div></div>)
-  } 
-  return(
-    <div>
-        {recitations.map((recitation, i) => (
-          <div css={{ padding: "10px" }} key={recitation.registrationNumber}>
-            <Section isOdd={!!(i % 2)} {...recitation} />
-          </div>
-        ))}
-    </div>
-  )
-}
 const SectionsList: React.FC<Props> = ({
   sections,
   displayDescription,
   displayNotes
 }) => {
+  if(!sections) {
+    return(<div></div>)
+  }
   return (
     <div>
       {sections.map((section, i) => (
         <div css={{ padding: "10px" }} key={section.registrationNumber}>
           <Section isOdd={!!(i % 2)} {...section} />
-          <RecitationList recitations = {section.recitations} />
+          <SectionsList 
+            sections = {section.recitations} 
+            displayDescription = {false}
+            displayNotes={false}
+          />
           {displayDescription && (
             <p>{section.description || "No description available"}</p>
           )}
