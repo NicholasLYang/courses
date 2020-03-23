b82cc10fb105ffa5d29051343dd1b4ac4c938104
Khanh Nguyen <44149581+Kn99HN@users.noreply.github.com>
2020-03-24 01:41:43 +0700

    Update with recitations (#10)
    * update with recitations
    
    * maybe this will be more readable
    
    * change from review

--- src/SectionsList.tsx
+++ src/SectionsList.tsx
@@ -5,7 +5,7 @@ import Section from "./Section";
 import { ISection } from "./types";
 
 interface Props {
-  sections: Array<ISection>;
+  sections: Array<ISection> | null; 
   displayDescription: boolean;
   displayNotes: boolean;
 }
@@ -15,11 +15,20 @@ const SectionsList: React.FC<Props> = ({
   displayDescription,
   displayNotes
 }) => {
+  if(!sections) {
+    return(<div></div>)
+  }
   return (
     <div>
       {sections.map((section, i) => (
         <div css={{ padding: "10px" }} key={section.registrationNumber}>
           <Section isOdd={!!(i % 2)} {...section} />
+          {section.recitations && 
+          <SectionsList 
+            sections={section.recitations} 
+            displayDescription = {false}
+            displayNotes = {false}
+          /> }
           {displayDescription && (
             <p>{section.description || "No description available"}</p>
           )}
